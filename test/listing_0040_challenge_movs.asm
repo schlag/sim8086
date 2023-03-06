40be6feab724ce7c85bc5957d351aeecda53ff90
schlag <rls@leesilvey.com>
2023-03-06 04:00:25 -0600

    add support for listings 39 and 40

--- test/listing_0040_challenge_movs.asm
+++ test/listing_0040_challenge_movs.asm
@@ -8,22 +8,31 @@
 ;
 ; Please see https://computerenhance.com for further information
 ;
-; ======================================================================== */
+; ========================================================================
 
 ; ========================================================================
-; LISTING 38
+; LISTING 40
 ; ========================================================================
 
 bits 16
 
-mov cx, bx
-mov ch, ah
-mov dx, bx
-mov si, bx
-mov bx, di
-mov al, cl
-mov ch, ch
-mov bx, ax
-mov bx, si
-mov sp, di
-mov bp, ax
+; Signed displacements
+mov ax, [bx + di - 37]
+mov [si - 300], cx
+mov dx, [bx - 32]
+
+; Explicit sizes
+mov [bp + di], byte 7
+mov [di + 901], word 347
+
+; Direct address
+mov bp, [5]
+mov bx, [3458]
+
+; Memory-to-accumulator test
+mov ax, [2555]
+mov ax, [16]
+
+; Accumulator-to-memory test
+mov [2554], ax
+mov [15], ax
